666f7d41e369f0858bfe8624c480b4665eeb4171
habibulalam <habibhassan.stt20@gmail.com>
2024-12-08 21:03:53 +0600

    Toggle Button's function added

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,8 @@
 //Donation and history Button functions
 
+const donationSection = document.getElementById("donation-section")
+const historySection = document.getElementById("history-section")
+
 const donateBtn = document.getElementById("donate-btn");
 const historyBtn = document.getElementById("history-btn");
 
@@ -10,20 +13,27 @@ donateBtn.addEventListener("click", function(){
     donateBtn.classList = activeButton ;
 
     historyBtn.classList = nonActiveButton ;
+    
+    donationSection.style.display = "block"; // Show Donation Section
+
+    historySection.style.display = "none" 
 })
 
 historyBtn.addEventListener("click" , function(){
     historyBtn.classList = activeButton;
 
     donateBtn.classList = nonActiveButton;
+
+    historySection.style.display = "block"
+
+    donationSection.style.display = "none"; // Show the div
+
 })
 
 
 
 
 // Donation calculations function
-
-
 function donationCalculation ( coinID , inputID){
     const myTotalAmount = parseInt(document.getElementById("my-initial-coins").innerText)
     const coin = parseInt(document.getElementById(coinID).innerText);
@@ -56,3 +66,27 @@ function donationCalculation ( coinID , inputID){
     // Clearing input field
     document.getElementById(inputID).value = '';
 }
+
+
+
+// Date and time setup
+function updateDateTime() {
+    const now = new Date();
+    const options = { 
+      weekday: 'long', 
+      year: 'numeric', 
+      month: 'long', 
+      day: 'numeric', 
+      hour: '2-digit', 
+      minute: '2-digit', 
+      second: '2-digit' ,
+      timeZoneName: 'short' 
+    };
+
+    // Format date and time
+    const dateTimeString = now.toLocaleString('en-US', options);
+
+    // Update the HTML
+    document.getElementById('dateTimeDisplay').textContent = dateTimeString;
+  }
+  updateDateTime();
